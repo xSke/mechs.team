89264941caa000340c3958023f0b103f489f73b9
Ske <voltasalt@gmail.com>
2021-11-23 09:10:56 +0100

    Back to one page per season

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,31 +7,24 @@ module.exports = {
           "/",
           "/core-lore/",
           "/players/",
-          "/team-history/full-history/",
-          /*{
+          {
             text: "Team History",
             children: [
-              {
-                text: "Season 12-14",
-                children: [
-                  "/team-history/season12",
-                  "/team-history/season13",
-                  "/team-history/season14",
-    
-                ]
-              },
-              "/team-history/season15",
-              "/team-history/season16",
-              "/team-history/season17",
-              "/team-history/season18",
-              "/team-history/season19",
-              "/team-history/season20",
-              "/team-history/season21",
-              "/team-history/season22",
-              "/team-history/season23",
-              "/team-history/season24",
+              "/team-history/season12/",
+              "/team-history/season13/",
+              "/team-history/season14/",
+              "/team-history/season15/",
+              "/team-history/season16/",
+              "/team-history/season17/",
+              "/team-history/season18/",
+              "/team-history/season19/",
+              "/team-history/season20/",
+              "/team-history/season21/",
+              "/team-history/season22/",
+              "/team-history/season23/",
+              "/team-history/season24/",
             ]
-          }*/
+          }
       ],
       contributors: false,
       lastUpdated: true,
